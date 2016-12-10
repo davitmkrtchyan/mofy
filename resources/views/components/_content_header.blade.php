869216313862f2e08b4fc0c4fa55b161b4605cfa
David Mkrtchyan <davo991@mail.ru>
2016-12-10 21:25:05 +0400

    Menu changes

--- resources/views/components/_content_header.blade.php
+++ resources/views/components/_content_header.blade.php
@@ -6,131 +6,49 @@
 
     <ul class="nav nav-list">
         <li class="hover">
-            <a href="index.html#" class="dropdown-toggle">
+            <a href="{{action('HomeController@matches')}}">
                 <i class="menu-icon fa fa-futbol-o"></i>
                 <span class="menu-text">
-                                    Odds Comparison
+                    <a href="{{action('HomeController@matches')}}">
+                        Matches
+                    </a>
                                 </span>
                 <b class="arrow fa fa-angle-down"></b>
             </a>
-            <b class="arrow"></b>
-            <ul class="submenu">
-                <li class="hover">
-                    <a href="{{action('HomeController@matches')}}">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Matches
-                    </a>
-                    <b class="arrow"></b>
-                </li>
-
-                <li class="hover">
-                    <a href="/">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Blocked Matches
-                    </a>
-                    <b class="arrow"></b>
-                </li>
-
-                <li class="hover">
-                    <a href="/">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Moving Margins
-                    </a>
-                    <b class="arrow"></b>
-                </li>
-                <li class="divider"></li>
-                <li class="hover">
-                    <a>
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Quick Payouts
-                    </a>
-
-                    <b class="arrow"></b>
-                </li>
-
-
-            </ul>
         </li>
 
         <li class="hover">
-            <a href="index.html#" class="dropdown-toggle">
+            <a href="{{action('HomeController@surebets')}}">
                 <i class="menu-icon fa fa-calculator"></i>
-                <span class="menu-text"> Surebets </span>
-                <b class="arrow fa fa-angle-down"></b>
-            </a>
-
-            <b class="arrow"></b>
-
-            <ul class="submenu">
-                <li class="hover">
-                    <a href="{{action('HomeController@surebets')}}">
-                        <i class="menu-icon fa fa-caret-right"></i>
+                <span class="menu-text">
+                 <a href="{{action('HomeController@surebets')}}">
                         Surebets
                     </a>
-                    <b class="arrow"></b>
-                </li>
-                <li class="hover">
-                    <a href="/">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Back/Lay
-                    </a>
-
-                    <b class="arrow"></b>
-                </li>
-                <li class="hover">
-                    <a href="/">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Middles
-                    </a>
-
-                    <b class="arrow"></b>
-                </li>
-                <li class="hover">
-                    <a href="/">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Value Bets
-                    </a>
-                    <b class="arrow"></b>
-                </li>
-            </ul>
+                 </span>
+            </a>
         </li>
 
         <li class="hover">
             <a href="/" class="dropdown-toggle">
                 <i class="menu-icon fa fa-list"></i>
-                <span class="menu-text">Bookmakers</span>
-
-                <b class="arrow fa fa-angle-down"></b>
-            </a>
-
-            <b class="arrow"></b>
-
-            <ul class="submenu">
-                <li class="hover">
-                    <a href="/bookmakers">
-                        <i class="menu-icon fa fa-caret-right"></i>
+                <span class="menu-text">
+                <a href="/bookmakers">
                         Bookmakers
                     </a>
-
-                    <b class="arrow"></b>
-                </li>
-
-                <li class="hover">
-                    <a href="/">
-                        <i class="menu-icon fa fa-caret-right"></i>
-                        Bonus
-                    </a>
-
-                    <b class="arrow"></b>
-                </li>
-            </ul>
+                </span>
+            </a>
         </li>
 
         <li class="hover">
-            <a href="http://moreoddsforyou.com/blog" target="_blank">
+            <a href="http://moreoddsforyou.com/blog">
                 <i class="menu-icon fa fa-pencil-square-o"></i>
+                <span class="menu-text">
+                <a href="http://moreoddsforyou.com/blog" target="_blank">
                 Blog
             </a>
+                </span>
+            </a>
+            
         </li>
 
     </ul>
